62cc252198bce0b097e9960d373c208f53b9fc46
Roston Smith <rostonlukesmith@gmail.com>
2025-10-08 10:45:41 +0200

    Task 2-4

--- EEE3096S SMTROS022 Pracs/Prac 4/Misc/Task 1/main.c
+++ EEE3096S SMTROS022 Pracs/Prac 4/Misc/Task 1/main.c
@@ -495,7 +495,8 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
+  lcd_init();
+  HAL_Delay(50); // Give LCD time to initialize
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -512,15 +513,16 @@ int main(void)
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
   // TODO: Start TIM3 in PWM mode on channel 3
-
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
   // TODO: Start TIM2 in Output Compare (OC) mode on channel 1
-
+  HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_1);
   // TODO: Start DMA in IT mode on TIM2->CH1. Source is LUT and Dest is TIM3->CCR3; start with Sine LUT
-
+  HAL_DMA_Start_IT(&hdma_tim2_ch1, (uint32_t)Sin_LUT, DestAddress, NS);
   // TODO: Write current waveform to LCD(Sine is the first waveform)
-
+  lcd_command(CLEAR);
+  lcd_putstring("Sine"); 
   // TODO: Enable DMA (start transfer from LUT to CCR)
-
+  __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
